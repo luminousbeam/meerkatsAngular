8bb2b3497330c4f72a887d3a4e47348ebaae49b6
luminousbeam <jocelyn.jeriah@gmail.com>
2020-01-18 20:07:17 -0500

    add Angular onto existing modularized express/node project

---
+++ client/public/src/app/http.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  constructor(private _http: HttpClient){
+    this.getMeerkats();
+  }
+  
+  getMeerkats(){
+    // our http response is an Observable, store it in a variable
+    let tempObservable = this._http.get('/meerkats');
+    // subscribe to the Observable and provide the code we would like to do with our data from the response
+    tempObservable.subscribe(data => console.log("Got our meerkats!", data));
+  }  
+}
+
---
+++ server/config/routes.js
@@ -0,0 +1,30 @@
+var meerkats = require('../controllers/meerkats.js');
+
+module.exports = function(app){
+    app.get('/meerkats', function(req, res) {
+        meerkats.show(req, res);
+    })
+
+    app.get('/meerkats/new', function(req, res) {
+        res.render('new');
+    })
+    
+    app.post('/add', function(req, res) {
+      meerkats.create(req, res);
+    })
+    
+    app.get('/meerkats/edit/:id', function(req, res) {
+        meerkats.edit(req, res);
+    })
+
+    app.post('/change/:id', function(req, res) {
+        meerkats.change(req, res); 
+    })
+    
+    app.post('/delete/:id', function(req,res){
+        meerkats.delete(req, res);
+    })
+    app.get('/meerkats/:id', function(req, res) {
+       meerkats.disp(req, res);
+    });
+}
